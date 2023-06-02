9f3ed14fcda09b0ab748c44a19ecbbf47664f43a
Steve Yee <steveplaysaxe@hotmail.com>
2023-06-02 12:58:40 -0700

    add bbq page

---
+++ src/routes/bbq.tsx
@@ -0,0 +1,14 @@
+import "./bbq.css";
+import BBQ from "~/components/BBQ/BBQ";
+
+export default function Home() {
+  return (
+    <main>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Dhurjati&display=swap"
+        rel="stylesheet"
+      ></link>
+      <BBQ />
+    </main>
+  );
+}
