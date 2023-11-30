b88bb2951246e03c27ba51ed49b9dac97ce9abce
Steve Yee <steveplaysaxe@hotmail.com>
2023-11-29 20:25:58 -0800

    link

--- src/components/LinkWord/LinkWord.tsx
+++ src/components/LinkWord/LinkWord.tsx
@@ -2,7 +2,7 @@ const LinkWord = ({ text, url }) => {
   const style = { color: "#ffbf00", textDecoration: "none" };
   return (
     <>
-      <a style={style} href={url}>
+      <a style={style} href={url} target="_blank">
         {`${text}`}
       </a>
     </>
